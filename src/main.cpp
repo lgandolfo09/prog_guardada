abbde1468e7a25adb41c14b4f6632895be777973
lgandolfo09 <lucasgamdolfo@gmail.com>
2024-06-12 21:33:21 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,64 @@
 #include <Arduino.h>
-#define LED1 16
-int a = 0;
-// the setup routine runs once when you press reset:
-void setup()
-{
-  pinMode(LED1, OUTPUT);
-  // initialize the digital pin as an output.
+
+int x = 0;
+void setup() {
   Serial.begin(9600);
+  Serial.println("Iniciando");
+  delay(1000);
+  x=1;
 }
 
-// the loop routine runs over and over again forever:
-void loop()
-{
-  Serial.println(a++);
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED1, HIGH);
-  // wait for a second
-  delay(2000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED1, LOW);
-  // wait for a second
-  delay(2000);
+void loop() {
+  switch (x){
+    case 1: 
+    Serial.println("Inicio");
+    delay(1000);
+    x=2;
+    break;
+    case 2: 
+    Serial.println("-Iniciar nueva medición");
+    Serial.println("-Registros");
+    delay(2000);
+    x=3;
+    break;
+    case 3: 
+    Serial.println("Ingrese nombre de usuario");
+    delay(2000);
+    x=4;
+    break;
+    case 4: 
+    Serial.println("Tiempo de medición:");
+    delay(2000);
+    x=5;
+    break;
+    case 5: 
+    Serial.print("Comenzar medición");
+    delay(2000);
+    x=6;
+    break;
+    case 6: 
+    Serial.println("Tire de la cuerda en:");
+    Serial.println("3");
+    delay(1000);
+    Serial.println("2");
+    delay(1000);
+    Serial.println("1");
+    delay(2000);
+    x=7;
+    break;
+    case 7: 
+    Serial.println("Temporizador");
+    delay(2000);
+    x=8;
+    break;
+    case 8: 
+    Serial.println("Nombre/Fecha");
+    Serial.println("Medición");
+    delay(2000);
+    x=9;
+    break;
+    case 9: 
+    Serial.println("Nombre/fecha");
+    break;
+  }
 }
